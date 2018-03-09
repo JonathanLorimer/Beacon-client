f8225031fb2459dc851a521ebda25e5bb5eecf74
Zushisan <74838@supinfo.com>
2018-03-09 17:46:13 +0000

    First template of working skeleton app waiting to receive data from an api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,84 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div id="test-form">
+          <form action="">
+
+            <input type="text" name="lat" value="Lat"/>
+
+            <input type="text" name="long" value="Long"/>
+
+            <input type="submit" value="Submit"/>
+          </form>
+        </div>
+
+    <div id="container">
+      <div id="left-section">
+
+        <h1>LEFT NAVIGATION DIV</h1>
+        <div class="categorie-list">
+
+        <p>GENERAL ACHIEVEMENT CATEGORIES (world/special/time based ?)</p>
+        <p>Each list is prob gonna a be a react component we loop through</p>
+        <p>LIST that will open and be pretty:</p>
+
+          <ul>
+            <li><b>Continents</b></li>
+              <li><i>North America</i></li>
+              <ul>
+                <li><b>Countries</b></li>
+                  <li><i>Canada</i></li>
+                  <ul>
+                    <li><b>Provinces/Regions</b></li>
+                      <li><i>Ontario</i></li>
+                      <ul>
+                        <li><b>Cities</b></li>
+                          <li><i>Toronto</i></li>
+                          <ul>
+                            <li><b>Neighbourhoods</b></li>
+                            <li><i>Downtown</i></li>
+                            <li><i>Midtown</i></li>
+                          </ul>
+                          <li><i>Ottawa</i></li>
+                          <li><i>etc...</i></li>
+                      </ul>
+                      <li><i>Quebec</i></li>
+                      <li><i>etc...</i></li>
+                  </ul>
+                  <li><i>Mexico</i></li>
+                  <li><i>etc...</i></li>
+              </ul>
+              <li><i>Asia</i></li>
+              <li><i>etc...</i></li>
+          </ul>
+        </div>
       </div>
+    </div>
+      <div id="middle-section">
+
+        <h1>MIDDLE OF PAGE</h1>
+        <div class="achievements-display">
+          <p>TITLE = Achievemnts group we are looking at</p>
+          <p>Each list is prob gonna a be a react component we loop through</p>
+
+
+          <ul>
+            <li>List of subcategory Achievements</li>
+            <li>///////////////</li>
+            <li>Down level achievement opens to show locations</li>
+          </ul>
+
+
+          </div>
+         </div>
+
+         <a href="">Diary</a>
+        </div>
+
     );
   }
 }
---
+++ src/components/Products.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Row, Col, PageHeader, Table } from 'react-bootstrap'
+import { Route, Switch, Link } from 'react-router-dom'
+
+// Product details modal dialog
+import ProductDetails from './ProductDetails'
+
+// Client-side model
+import Resource from '../models/resource'
+const ProductStore = Resource('products')
+
+
+class Products extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: [],
+      selectedProduct: {},
+      showDetails: false,
+      errors: null
+    }
+  }
+
+  componentWillMount() {
+    ProductStore.findAll() // ProductStore does the API fetching!
+      .then((result) => this.setState({ products: result.data, errors: null }))
+      .catch((errors) => this.setState({ errors: errors }))
+  }
+
+  render() {
+    return (
+      <Row>
+        <Col xs={12}>
+
+          <PageHeader>
+            Products
+          </PageHeader>
+
+          <Table>
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {this.state.products.map((product, index) => (
+                <tr key={index}>
+                  <td>{product.id}</td>
+                  <td>
+                    {/* <Link> is a react-router component that works pretty much like <a href> */}
+                    <Link to={`/products/${product.id}`}>
+                      {product.name}
+                    </Link>
+                  </td>
+                  <td>{product.price}</td>
+                  <td>{product.quantity}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+
+          {/* If the URL has an id at the end, we show the details dialog */}
+          <Switch>
+            <Route path="/products/:id" component={ProductDetails} />
+          </Switch>
+
+        </Col>
+      </Row>
+    )
+  }
+}
+
+export default Products
