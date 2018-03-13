8b2ca0bd9428141be7fbe7c35b1215b376376dae
Zushisan <74838@supinfo.com>
2018-03-13 00:23:40 +0000

    added fake coord

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Navbar, Nav, NavItem } from 'react-bootstrap'
 
+import Resource from '../models/resource'
+
+const Coordinates = Resource('coordinates')
+
 const TopNav = (props) => (
   <Navbar>
     <Navbar.Header>
@@ -21,8 +25,11 @@ const TopNav = (props) => (
         <Link to="/diary">Diary</Link>
       </NavItem>
     </Nav>
-
   </Navbar>
 )
 
+Coordinates.create({ latitude: "43.6425662", longitude: "-79.3892455" })
+
 export default TopNav
+
+ 
--- src/models/api.js
+++ src/models/api.js
@@ -17,7 +17,8 @@ const server = axios.create({
   baseURL: BASEURL,
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/vnd.api+json'
+    'Content-Type': 'application/vnd.api+json',
+    'Accept': 'application/json'
   }
 })
 
