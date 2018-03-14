26d1bc95d665a25b08854762cee217d56909e698
Zushisan <74838@supinfo.com>
2018-03-14 20:59:46 +0000

    fake coord form setup

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,10 +1,29 @@
 import React , { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { Navbar, Nav, NavItem } from 'react-bootstrap'
+import Resource from '../models/resource'
+const Coordinates = Resource('coordinates')
 
 class TopNav extends Component {
   constructor(props){
-   super(props)
+    super(props)
+    this.state = {
+      status: "",
+      errors: null
+    }
+  }
+
+  handleSubmit = data => {
+    if (isNaN(this.state.latitude) || isNaN(this.state.longitude)) {
+      this.setState({ status: "Invalid Coordinates" })
+    }
+    else {
+      Coordinates.create({ latitude: this.state.latitude, longitude: this.state.longitude })
+        .then((result) => {
+          this.setState({ status: result.data.data, errors: null })
+        })
+        .catch((errors) => this.setState({ errors: errors }))      
+    }
   }
 
 render(){
@@ -15,7 +34,7 @@ render(){
         <Navbar.Header>
           <Navbar.Brand>
            <Link to='/'>Beacon</Link>
-            
+
           </Navbar.Brand>
         </Navbar.Header>
 
@@ -33,9 +52,29 @@ render(){
           </NavItem>
         </Nav>
 
+          <p> {this.state.status} </p>
+          <div class="input-coord">
+            <input type="text" placeholder="latitude" name="latitude" onChange={event => {
+              this.setState({ latitude: event.target.value })
+            }} />
+          <input type="text" placeholder="longitude" name="longitude" onChange={event => {
+              this.setState({ longitude: event.target.value })
+            }} />
+            <input type="submit" value="send" onClick={event => {
+              console.log(this.state.latitude)
+              console.log(this.state.longitude)
+              this.handleSubmit()
+            }} />
+          </div>
+
+
       </Navbar>
     )
   }
 }
 export default TopNav
-// { Coordinates.create({ latitude: "43.6425662", longitude: "-79.3892455" }) }
+
+// Coordinates.create({ latitude: "51.07545", longitude: "-113.95276" })
+// Coordinates.create({ latitude: "43.6425662", longitude: "-79.3892455" })
+// Coordinates.create({ latitude: "51.07545", longitude: "-113.95276" })
+// Coordinates.create({ latitude: "43.6425662", longitude: "-79.3892455" })
