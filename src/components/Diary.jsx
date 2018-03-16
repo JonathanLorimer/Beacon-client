306fb90e7158083f790e8b9a87d7fbf29a519fb1
Jonathan Lorimer <jonathan_lorimer@mac.com>
2018-03-16 17:58:46 +0000

    setup basic diary functionality

--- src/components/Diary.jsx
+++ src/components/Diary.jsx
@@ -4,55 +4,47 @@ import { Route, Redirect, Switch, Link } from 'react-router-dom'
 
 // Client-side model
 import Resource from '../models/resource'
-const ClientStore = Resource('users')
+const Locations = Resource('users', 'diaries')
 
 class Diary extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      clients: [],
+      locations: [],
       errors: null
 
     }
   }
 
   componentWillMount() {
-    ClientStore.findAll()
-      .then((result) => this.setState({ clients: result.data }))
+    Locations.findAllChildren(1)
+      .then((result) => {console.log(result.data); this.setState({ locations: result.data })})
       .catch((errors) => this.setState({ errors: errors }))
+  }
+
+  listPresenter() {
+    const list = this.state.locations.map((location) => {
+        return (
+          <tr className="diary entry">
+            <td>{location.name}</td>
+            <td>{location.created_at}</td>
+            <td>{location.category}</td>
+          </tr>)
+    })
+    return list
   }
 
   render() {
     if(this.props.auth && this.props.currentUser.data !=="failed"){
       return (
-        <Row>
-          <Col xs={12}>
-
-            <PageHeader>
-              Clients
-            </PageHeader>
-
-            <Table>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Name</th>
-                  <th>Email</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                {this.state.clients.map((client, index) => (
-                  <tr key={index}>
-                    <td>{client.id}</td>
-                    <td>{client.name}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-      )
+      <table>
+        <tr>
+          <th>Location</th>
+          <th>Date Achieved</th>
+          <th>Type</th>
+        </tr>
+        {this.listPresenter()}
+      </table>)
     } else {
        return <Redirect to='/userlogin'/>
     }
